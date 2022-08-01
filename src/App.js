82d035faf3ce1d5bd707fe2d2bfd88ac06ac1ce3
suyash460 <71191009+suyash460@users.noreply.github.com>
2022-08-01 10:41:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello Fraands</div>;
+  return <div className="App">Hello Suyash</div>;
 }
 
 export default App;
