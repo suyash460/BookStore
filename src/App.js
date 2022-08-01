4f3e9ffc0f16c8831f6a8dbdbb1754c92e9bc7c7
Suyash Sargar <suyash.sargar@thoughtworks.com>
2022-08-01 11:19:49 +0530

    Add. Login Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Login from "./Login";
 
 function App() {
-  return <div className="App">Hello Thoughtworks</div>;
+  return (
+    <>
+      <div className="App">Hello Thoughtworks</div>;
+      <Login />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <>Hiiii</>;
+};
+
+export default Login;
